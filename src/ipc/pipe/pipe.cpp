87eda04b9c940b03bfdf81cd07da23ab4e39b1c5
rchetia <rchetia1228@gmail.com>
2022-07-20 00:01:29 +0530

    Updating signalling between process using pipe

--- src/ipc/pipe/pipe.cpp
+++ src/ipc/pipe/pipe.cpp
@@ -1,11 +1,27 @@
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <iostream>
 
 #define READ_END 0
 #define WRITE_END 1
 
-int main() {
+#define SLEEP_AT_PARENT 0
+#define SLEEP_AT_CHILD  1
+
+/*
+ * Send primitive data between processes.
+ */
+/*int send_data_between_proc() {
+
+}*/
+
+/*
+ * Demo program to demonstrate signalling between parent and child.
+ * Child (do some operation) -> Signal Parent about completion of operation->
+ * parent wakes up and exits.
+ */
+int signal_between_parentchild(int where_to_sleep) {
   int fd[2];
   int result_dummy = 0;
   pipe(fd);
@@ -13,19 +29,42 @@ int main() {
   cPid = fork();
   if (cPid == 0) {  // child
     close(fd[READ_END]);
+    if (where_to_sleep == SLEEP_AT_CHILD) {
+      sleep(3); // Sleep as placeholder for some operations
+    }
     std::cout << "Hello From Child" << std::endl;
-    write(fd[WRITE_END], &result_dummy, sizeof(result_dummy));
+    write(fd[WRITE_END], &result_dummy, sizeof(result_dummy)); // write blocks till all data is consumed @read end
     close(fd[WRITE_END]);
     exit(0);
   } else if (cPid > 0) {  // parent
+    if (where_to_sleep == SLEEP_AT_PARENT) {
+      sleep(3); // Sleep as placeholder for some operations
+    }
     close(fd[WRITE_END]);
-    read(fd[READ_END], &result_dummy, sizeof(result_dummy));
+    read(fd[READ_END], &result_dummy, sizeof(result_dummy)); // read blocks till data is available @write end
     close(fd[READ_END]);
     std::cout << "Hello From Parent" << std::endl;
     wait(NULL);
   } else {
     printf("fork() failed\n");
     return -1;
+  }
+  return 0;
+}
+
+/*
+ * main test.
+ */
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    return -1;
+  }
+  int myarg = atoi(argv[1]);
+  if (0 == myarg) {
+    return signal_between_parentchild(SLEEP_AT_CHILD);
+  } else if (1 == myarg) {
+    return signal_between_parentchild(SLEEP_AT_PARENT);
+  } else {
   }
   return 0;
 }
